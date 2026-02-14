08492d8e1301d85d0925f97a6b7d92a1ace8d232
rgardler-msft <ross.gardler@microsoft.com>
2026-02-14 01:10:53 -0800

    mods: Reduce wave 2-3 alien speeds
    - BlueWaveAlien: Speed reduced to 105% (5% faster than default)
    - GreenSniperAlien: Speed reduced to 110% (10% faster than default)

--- mods/blue_wave/__init__.py
+++ mods/blue_wave/__init__.py
@@ -15,7 +15,7 @@ class BlueWaveAlien(AlienBase):
     """Blue alien with higher shooting rate but slower movement."""
 
     # Override class constants for custom behavior
-    DEFAULT_SPEED = 1.25  # Move at 125% of default speed (25% faster)
+    DEFAULT_SPEED = 1.05  # Move at 105% of default speed (5% faster)
     DEFAULT_SHOOT_CHANCE = 0.005  # 5x higher shooting rate
     DEFAULT_BULLET_SPEED = 6.0
     DEFAULT_COLOR = (0, 0, 255)  # Bright blue
